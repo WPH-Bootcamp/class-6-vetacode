ce94feb0500002cdeb48a1036739a575498a1c17
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2024-11-17 22:27:28 +0700

    Assignment 6 Submit 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,14 @@
-// const { src, dest, watch, series } = require('gulp');
-// const sass = require('gulp-sass')(require('sass'));
-
-// function buildStyles() {
-//   return src('scss/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(dest('css'));
-// }
-
-// function watchTask() {
-//   watch(['scss/**/*.scss'], buildStyles);
-// }
-
-// exports.default = series(buildStyles, watchTask);
-
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return gulp
-    .src('scss/**/*.scss')
+  return src('scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(dest('css'));
 }
 
-exports.default = gulp.series(buildStyles);
+function watchTask() {
+  watch(['scss/**/*.scss'], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
